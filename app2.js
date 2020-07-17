e1e0e4fd702b30fe5a920aeb3afcc69b56ced9c9
megan <msmeganmcmanus@gmail.com>
2020-07-17 08:14:09 -0700

    disclaimer and time info added

--- app2.js
+++ app2.js
@@ -64,7 +64,7 @@ function makeLaunchLI(launch) {
 	const mission = document.createElement('B');
 	mission.innerText = launch.mission_name;
 	newLI.append(mission);
-	newLI.innerHTML += ` - ${launch.rocket.rocket_name}`;
+	newLI.innerHTML += ` - flight number ${launch.flight_number}, at ${launch.launch_date_local},`;
 	return newLI;
 }
 
